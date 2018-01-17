956cd8103ef975531a0d08d1dfe8963662e6c1f2
lonewalker72 <walkisai.72@gmail.com>
2018-01-17 05:43:56 -0800

    testing #wordFreq

--- script/app.js
+++ script/app.js
@@ -1,8 +1,17 @@
-function openFile(e) {
-  let input = e.target;
-  let reader = new FileReader();
-  reader.onload = function() {
-    let str = reader.result;
-  };
-  reader.readAsText(input.files[0]);
-}
+module.exports = {
+
+  loadFile(e) {
+    let input = e.target;
+    let reader = new FileReader();
+    reader.onload = function() {
+      let str = reader.result;
+    };
+    reader.readAsText(input.files[0]);
+  },
+
+  mkArr(str) {
+    let arr = str.split(' ');
+    return arr;
+  },
+
+};
--- test/test.js
+++ test/test.js
@@ -8,23 +8,53 @@ describe('app', function() {
   // Need to figure out how to replicate event or alternative approach.
   describe.skip('#loadFile', function() {
     let onChangeSuccessSpy;
+
     before(function() {
       onChangeSuccessSpy = sinon.spy();
       document.getElementsByTagName('input').trigger('onChangeSuccess', onChangeSuccessSpy);
     });
+
     it('Should trigger an event', function() {
       document.getElementsByTagName('input').onchange();
       expect(onChangeSuccessSpy.callCount).to.equal(1);
     });
 
     it('Should return a string', function() {
-      expect(app.loadFile(), 'string');
+      expect(app.loadFile()).to.be.a('string');
     });
   });
 
   describe('#mkArr', function(){
     it('Should return an array given a string', function() {
-      expect(app.mkArr(), 'string');
+      let str = 'i spy with my eye';
+      expect(app.mkArr(str)).to.be.an('array');
+    });
+
+    it('Should return an array of words', function() {
+      let str = 'I like fishing';
+      assert.equal(app.mkArr(str).indexOf('like'), 1);
+    });
+  });
+
+  describe('#wordFreq', function() {
+    it('Should return and object give an array', function() {
+      let arr = ['hi', 'hi', 'hi', 'the', 'the', 'the', 'red', 'green', 'yellow', 'yellow'];
+      expect(app.wordFreq()).to.be.an('object');
+    });
+
+    it('Should return { word: num of occurances }', function() {
+      let arr = ['one', 'one', 'one', 'three', 'three', 'three', 'three', 'is', 'is', 'is', 'a', 'cat', 'dog', 'dog'];
+      assert.isObject(app.wordFreq(arr), '{ one: 3, three: 4, is: 3, a: 1 }');
+    });
+
+    it('Should return an array of non duplicate words', function() {
+      let arr = ['ok', 'ok', 'ok', 'dice', 'dice', 'is', 'is', 'at', 'red', 'red', 'red', 'red'];
+      assert.equal(app.wordFreq(arr).indexOf('ok'), 0);
+      assert.equal(app.wordFreq(arr).indexOf('dice'), 1);
+      assert.equal(app.wordFreq(arr).indexOf('is'), 2);
+      assert.equal(app.wordFreq(arr).indexOf('at'), 3);
+      assert.equal(app.wordFreq(arr).indexOf('red'), 4);
+
     });
   });
 
