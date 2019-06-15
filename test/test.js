88e0be70d67481fd1f26c8ae3c3a8aa106cef749
lonewalker72 <walkisai.72@gmail.com>
2019-06-14 20:04:40 -0500

    updated dependencies and addressed security vulnerablilities

--- test/test.js
+++ test/test.js
@@ -1,28 +1,8 @@
 const assert = require('chai').assert;
 const expect = require('chai').expect;
-const sinon = require('sinon');
 const app = require('../script/app');
 
 describe('app', function() {
-
-  // Need to figure out how to replicate event or alternative approach.
-  describe.skip('#loadFile', function() {
-    let onChangeSuccessSpy;
-
-    before(function() {
-      onChangeSuccessSpy = sinon.spy();
-      document.getElementsByTagName('input').trigger('onChangeSuccess', onChangeSuccessSpy);
-    });
-
-    it('Should trigger an event', function() {
-      document.getElementsByTagName('input').onchange();
-      expect(onChangeSuccessSpy.callCount).to.equal(1);
-    });
-
-    it('Should return a string', function() {
-      expect(app.loadFile()).to.be.a('string');
-    });
-  });
 
   describe('#mkArr', function(){
     it('Should return an array given a string', function() {
